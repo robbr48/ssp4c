e3881443afed91e5eb65bf28357e1ee4a0f1cd25
Robert Braun <robert.braun@liu.se>
2025-05-13 09:26:46 +0200

    Add XML namespace to parser function names

--- include/ssp4c_xml_parsers.h
+++ include/ssp4c_xml_parsers.h
@@ -5,19 +5,19 @@
 #include "ssp4c_public.h"
 
 bool parseSsd(sspHandle *ssp, ssdHandle *ssd, const char* path);
-bool parseConnectorsElement(ezxml_t element, ssdConnectorsHandle* h, sspHandle *ssp);
-bool parseConnectorElement(ezxml_t element, ssdConnectorHandle *h, sspHandle *ssp);
-bool parseComponentsElement(ezxml_t element, ssdComponentsHandle* h, sspHandle *ssp);
-bool parseComponentElement(ezxml_t element, ssdComponentHandle* h, sspHandle *ssp);
-bool parseParameterBindingsElement(ezxml_t element, ssdParameterBindingsHandle* h, sspHandle *ssp);
-bool parseParameterBindingElement(ezxml_t element, ssdParameterBindingHandle *h, sspHandle *ssp);
+bool parseSsdConnectorsElement(ezxml_t element, ssdConnectorsHandle* h, sspHandle *ssp);
+bool parseSsdConnectorElement(ezxml_t element, ssdConnectorHandle *h, sspHandle *ssp);
+bool parseSsdComponentsElement(ezxml_t element, ssdComponentsHandle* h, sspHandle *ssp);
+bool parseSsdComponentElement(ezxml_t element, ssdComponentHandle* h, sspHandle *ssp);
+bool parseSsdParameterBindingsElement(ezxml_t element, ssdParameterBindingsHandle* h, sspHandle *ssp);
+bool parseSsdParameterBindingElement(ezxml_t element, ssdParameterBindingHandle *h, sspHandle *ssp);
 bool parseSsdParameterValuesElement(ezxml_t element, ssdParameterValuesHandle* handle, sspHandle *ssp);
-bool parseParameterMappingElement(ezxml_t element,ssdParameterMappingHandle* handle, sspHandle *ssp);
+bool parseSsdParameterMappingElement(ezxml_t element,ssdParameterMappingHandle* handle, sspHandle *ssp);
 bool parseSsmParameterMappingElement(ezxml_t element, ssmParameterMappingHandle *handle, sspHandle *ssp);
 bool parseSsmMappingEntryElement(ezxml_t element, ssmParameterMappingEntryHandle *handle, sspHandle *ssp);
 bool parseSscTransformationChoiceElement(ezxml_t element, sscMappingTransformHandle *handle, sspHandle *ssp);
 bool parseSscMapEntryElement(ezxml_t element, sscMapEntryHandle *handle, sspHandle *ssp);
-bool parseParameterSetElement(ezxml_t element, ssvParameterSetHandle* parameterSetHandle, sspHandle *ssp);
-bool parseParameterElement(ezxml_t element, ssvParameterHandle* parameterHandle, sspHandle *ssp);
+bool parseSsvParameterSetElement(ezxml_t element, ssvParameterSetHandle* parameterSetHandle, sspHandle *ssp);
+bool parseSsvParameterElement(ezxml_t element, ssvParameterHandle* parameterHandle, sspHandle *ssp);
 
 #endif // SSP4C_XML_PARSERS_H
