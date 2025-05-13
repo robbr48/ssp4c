e7fc06f2d040263bfb3500dfe65ec85be740a113
Robert Braun <robert.braun@liu.se>
2025-05-13 09:59:14 +0200

    Store ezxml element in each handle object

--- include/ssp4c_xml_parsers.h
+++ include/ssp4c_xml_parsers.h
@@ -9,15 +9,16 @@ bool parseSsdConnectorsElement(ezxml_t element, ssdConnectorsHandle* h, sspHandl
 bool parseSsdConnectorElement(ezxml_t element, ssdConnectorHandle *h, sspHandle *ssp);
 bool parseSsdComponentsElement(ezxml_t element, ssdComponentsHandle* h, sspHandle *ssp);
 bool parseSsdComponentElement(ezxml_t element, ssdComponentHandle* h, sspHandle *ssp);
+bool parseSsdElementGeometryElement(ezxml_t element, ssdElementGeometryHandle *h, sspHandle *ssp);
 bool parseSsdParameterBindingsElement(ezxml_t element, ssdParameterBindingsHandle* h, sspHandle *ssp);
 bool parseSsdParameterBindingElement(ezxml_t element, ssdParameterBindingHandle *h, sspHandle *ssp);
 bool parseSsdParameterValuesElement(ezxml_t element, ssdParameterValuesHandle* handle, sspHandle *ssp);
 bool parseSsdParameterMappingElement(ezxml_t element,ssdParameterMappingHandle* handle, sspHandle *ssp);
-bool parseSsmParameterMappingElement(ezxml_t element, ssmParameterMappingHandle *handle, sspHandle *ssp);
-bool parseSsmMappingEntryElement(ezxml_t element, ssmParameterMappingEntryHandle *handle, sspHandle *ssp);
-bool parseSscTransformationChoiceElement(ezxml_t element, sscMappingTransformHandle *handle, sspHandle *ssp);
-bool parseSscMapEntryElement(ezxml_t element, sscMapEntryHandle *handle, sspHandle *ssp);
+bool parseSsmParameterMappingElement(ezxml_t element, ssmParameterMappingHandle *h, sspHandle *ssp);
+bool parseSsmMappingEntryElement(ezxml_t element, ssmParameterMappingEntryHandle *h, sspHandle *ssp);
+bool parseSscTransformationChoiceElement(ezxml_t element, sscMappingTransformHandle *h, sspHandle *ssp);
+bool parseSscMapEntryElement(ezxml_t element, sscMapEntryHandle *h, sspHandle *ssp);
 bool parseSsvParameterSetElement(ezxml_t element, ssvParameterSetHandle* parameterSetHandle, sspHandle *ssp);
-bool parseSsvParameterElement(ezxml_t element, ssvParameterHandle* parameterHandle, sspHandle *ssp);
+bool parseSsvParameterElement(ezxml_t element, ssvParameterHandle* h, sspHandle *ssp);
 
 #endif // SSP4C_XML_PARSERS_H
